650469d6bd6a95c016b9202b113b2a61acf49fd0
RubenLCgit <125395496+RubenLCgit@users.noreply.github.com>
2023-03-16 21:21:05 +0100

    add: add methods to access and search for products

--- Selenium/src/main/java/BaseTest/BaseTest.java
+++ Selenium/src/main/java/BaseTest/BaseTest.java
@@ -1,5 +1,6 @@
 package BaseTest;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -7,11 +8,24 @@ public class BaseTest {
 
     private WebDriver driver;
 
-    public void setup(){
+    public void setup() /*throws InterruptedException*/ {
         System.setProperty("webdriver.http.factory", "jdk-http-client");
         driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.get("https://www.lidl.es/");
+
+        //Thread.sleep(3000); Sirve para añadir un tiempo de demora en la ejecución
+
+        //driver.quit(); Metodo de la clase WebDriver para cerrar el objeto o instancia.
+    }
+
+    public void aceptCookie(){
+        driver.findElement(By.xpath("//button[@class='cookie-alert-extended-button']")).click();
+    }
+
+    public void searchProduct(){
+        driver.findElement(By.cssSelector("input#mainsearch-input")).sendKeys("zumo");
+        driver.findElement(By.cssSelector("button.primary")).click();
     }
 
 }
--- Selenium/src/main/java/org/selenium/Main.java
+++ Selenium/src/main/java/org/selenium/Main.java
@@ -3,9 +3,11 @@ package org.selenium;
 import BaseTest.BaseTest;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) /*throws InterruptedException*/ {
 
         BaseTest test = new BaseTest();
         test.setup();
+        test.aceptCookie();
+        test.searchProduct();
     }
 }
